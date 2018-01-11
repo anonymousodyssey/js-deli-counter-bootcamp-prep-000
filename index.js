d73862ec530c60a70b481bbe4457cd0d9b900a0e
anonymousodyssey <jeffte.80@gmail.com>
2018-01-11 02:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing() {
 }
 
 function currentLine(katzDeliLine) {
-  var currentLine = []
+  var currentLine = [];
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
